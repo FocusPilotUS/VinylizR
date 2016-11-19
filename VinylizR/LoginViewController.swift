8566f81ac28570b218e867c5c1c674ba91363839
FocusPilotUS <nsturner09@gmail.com>
2016-11-18 20:34:29 -0700

    Working on intro

--- VinylizR/LoginViewController.swift
+++ VinylizR/LoginViewController.swift
@@ -7,8 +7,43 @@
 //
 
 import Foundation
-import Material
+import AVFoundation
 
 class LoginViewController: ViewController {
+    var player: AVPlayer?
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loadVideo()
+    }
+    
+    private func loadVideo() {
+        //this line is important to prevent background music stop
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryAmbient)
+        } catch { }
+        
+        let path = Bundle.main.path(forResource: "recordspinning",
+                                                         ofType:"mp4")
+        
+        player = AVPlayer(url: NSURL(fileURLWithPath: path!) as URL)
+        let playerLayer = AVPlayerLayer(player: player)
+        
+        playerLayer.frame = self.view.frame
+        playerLayer.videoGravity = AVLayerVideoGravityResizeAspectFill
+        playerLayer.zPosition = -1
+        playerLayer.opacity = 0.5
+        
+        self.view.layer.addSublayer(playerLayer)
+        
+        player?.seek(to: kCMTimeZero)
+        player?.play()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(self.setVideoToStart), name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: player?.currentItem)
+    }
+    
+    func setVideoToStart() {
+        player?.seek(to: kCMTimeZero)
+    }
 }
